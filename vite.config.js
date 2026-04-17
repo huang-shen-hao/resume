8912aa187eec644776bfb33f8846ddbdb075b7c4
tuandui <tuandui@tsign.cn>
2026-04-17 19:01:10 +0800

    feat: 修改根目录

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // 关键修改：设置基础路径
-  base: '/resume/',
+  base: '/',
   build: {
     outDir: 'dist'
   }
